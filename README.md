d82da6267688fd0216859285984ef3cdadb7f032
dmodena <d.modena@live.com>
2016-08-10 16:28:21 -0300

    Melhorias nos locais de arquivo e instalação de pacotes.

--- README.md
+++ README.md
@@ -2,8 +2,6 @@ pug-test
 =========
 Um projeto de exemplo de compilação de arquivos template pug usando Gulp js.
 
-Para instalar as dependências
------------------------------
-- `npm install gulp`
-- `npm install gulp-pug`
-- `npm install gulp-sass`
+Para instalar as dependências: `npm install`.
+
+Para iniciar o watch: `gulp`.
--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,29 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var sass = require('gulp-sass');
 
+var locations = {
+  "in": "./",
+  "out": "./",
+  "copy": "./dist/"
+};
+
 gulp.task('default', ['watch', 'views', 'styles']);
 
 gulp.task('watch', function() {
-    gulp.watch('./*.pug', ['views']);
-    gulp.watch(['./*.sass', './*.scss'], ['styles']);
+  gulp.watch(locations.in + '*.pug', ['views']);
+  gulp.watch([locations.in + '*.sass', locations.in + '*.scss'], ['styles']);
 });
 
 gulp.task('views', function() {
-    return gulp.src('./*.pug')
+  return gulp.src(locations.in + '*.pug')
     .pipe(pug({locals: {}}))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest(locations.out))
+    .pipe(gulp.dest(locations.copy));
 });
 
 gulp.task('styles', function() {
-    return gulp.src(['./*.sass', './*.scss'])
+  return gulp.src([locations.in + '*.sass', locations.in + '*.scss'])
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
-})
+    .pipe(gulp.dest(locations.out))
+    .pipe(gulp.dest(locations.copy));
+});
